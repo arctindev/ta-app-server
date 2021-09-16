80317b8b65fe764cdde6ddab5c3fe8ca9983f156
arctindev <arctindev@gmail.com>
2021-09-17 01:43:57 +0200

    Created basic mocks for future database model developing

---
+++ src/data/userData.ts
@@ -0,0 +1,20 @@
+import { historyData } from './history';
+
+export const userData = [
+    {
+        id: 1,
+        token: 'asdasdasd',
+        name: "lukas",
+        password: "oioioioi",
+        email: 'lukas@lukas.com',
+        data: historyData,
+    },
+    {
+        id: 1,
+        token: 'testtokenmanhehe',
+        name: "test",
+        password: "testtest",
+        email: 'test@lukas.com',
+        data: [],
+    }
+]
